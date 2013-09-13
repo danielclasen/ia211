634292ba68951b048e6eed75f29b2547f05fc62c
Daniel Clasen <mail@danielclasen.de>
2013-09-13 10:23:59 +0200

    Added some controllers and views. Also added twitter-bootstrap and font-awesome for styling

---
+++ VersionR/VersionR/Controllers/ServiceUserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VersionR.Controllers
+{
+    public class ServiceUserController : Controller
+    {
+        //
+        // GET: /ServiceUser/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- VersionR/VersionR/Views/Home/Index.aspx
+++ VersionR/VersionR/Views/Home/Index.aspx
@@ -2,6 +2,10 @@
 
 <script runat="server">
 
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+
+    }
 </script>
 <asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
     Startseite
