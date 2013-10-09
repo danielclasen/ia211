55df20d72aaa99f58201e73b43e6e1c4d7877f08
Marios Christodoulou <hansnans@gmail.com>
2013-10-09 12:46:00 +0200

    repositories eingebaut beispielanwenung in AccountController.cs

---
+++ VersionR/VersionR/DAL/ICostumerModulesRepo.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Linq.Expressions;
+//using VersionR.Models;
+using System;
+
+namespace VersionR.DAL
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        internal VersionRContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(VersionRContext context)
+        {
+            //this.context = new VersionRContext(objContext);
+            this.context = context;
+            dbSet = this.context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<System.Func<TEntity, bool>> filter = null,
+            System.Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null)
+        {
+            IQueryable<TEntity> query = dbSet;
+            
+            
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+                
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+        public virtual void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            //falls die Zeile gerade hinzugefügt wurde aber noch nicht attached (also "commited")
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        internal object Get(object filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VersionR/VersionR/DAL/Repositories.cs
@@ -0,0 +1,125 @@
+﻿using VersionR.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VersionR.DAL
+{
+    public class Repositories
+    {
+        private VersionRContext context = new VersionRContext(new VersionR.Models.VersionR());
+
+        private GenericRepository<Customer_Modules> customerModulesRepoistory;
+        private GenericRepository<Download> downloadRepoistory;
+        private GenericRepository<Manual> manualRepoistory;
+        private GenericRepository<Module> moduleRepoistory;
+        private GenericRepository<Role> roleRepoistory;
+        private GenericRepository<User> userRepoistory;
+        private GenericRepository<Version> versionRepoistory;
+
+        private bool disposed = false;
+
+        public GenericRepository<Download> DownloadRepoistory
+        {
+            get
+            {
+                if (downloadRepoistory == null)
+                {
+                    downloadRepoistory = new GenericRepository<Download>(context);
+                }
+                return downloadRepoistory;
+            }
+        }
+
+        public GenericRepository<Manual> ManualRepoistory
+        {
+            get
+            {
+                if (manualRepoistory == null)
+                {
+                    manualRepoistory = new GenericRepository<Manual>(context);
+                }
+                return manualRepoistory;
+            }
+        }
+
+        public GenericRepository<Module> ModuleRepoistory
+        {
+            get
+            {
+                if (moduleRepoistory == null)
+                {
+                    moduleRepoistory = new GenericRepository<Module>(context);
+                }
+                return moduleRepoistory;
+            }
+        }
+
+        public GenericRepository<Role> RoleRepoistory
+        {
+            get
+            {
+                if (roleRepoistory == null)
+                {
+                    roleRepoistory = new GenericRepository<Role>(context);
+                }
+                return roleRepoistory;
+            }
+        }
+
+        public GenericRepository<User> UserRepoistory
+        {
+            get
+            {
+                if (userRepoistory == null)
+                {
+                    userRepoistory = new GenericRepository<User>(context);
+                }
+                return userRepoistory;
+            }
+        }
+
+
+        public GenericRepository<Version> VersionRepoistory
+        {
+            get
+            {
+                if (versionRepoistory == null)
+                {
+                    versionRepoistory = new GenericRepository<Version>(context);
+                }
+                return versionRepoistory;
+            }
+        }
+
+        public GenericRepository<Customer_Modules> CustomerModulesRepoistory
+        {
+            get
+            {
+                if (customerModulesRepoistory == null)
+                {
+                    customerModulesRepoistory = new GenericRepository<Customer_Modules>(context);
+                }
+                return customerModulesRepoistory;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed && disposing)
+            {
+                context.Dispose();
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            System.GC.SuppressFinalize(this);
+        }
+
+    }
+}
