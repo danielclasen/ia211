5b9589e29c05009fcc708bed3e8f29a96e39bb54
Marios Christodoulou <hansnans@gmail.com>
2013-10-09 10:45:50 +0200

    EntityFramework Problem fixed Weitere dlls eingebunden! (packages/entityFramework...)

---
+++ VersionR/VersionR/DAL/VersionRContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace VersionR.DAL
+{
+    public class VersionRContext : DbContext
+    {
+        public VersionRContext(VersionR.Models.VersionR objectContext)
+            : base(objectContext, false)
+        {
+
+        }
+    }
+}
