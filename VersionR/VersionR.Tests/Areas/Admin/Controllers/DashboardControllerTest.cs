dc300fc2c2041e9c5b3b54f0db75a17075a15f44
Christopher Zantopp <dark.funeral.1@web.de>
2013-10-15 21:00:53 +0200

    Testklassen hinzugefügt (funktionieren noch nicht)

---
+++ VersionR/VersionR.Tests/Areas/Admin/Controllers/DashboardControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionR;
+using VersionR.Areas.Admin.Controllers;
+
+namespace VersionR.Tests.Areas.Admin.Controllers
+{
+    [TestClass]
+    public class DashboardControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            DashboardController controller = new DashboardController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ VersionR/VersionR.Tests/Areas/Admin/Controllers/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionR;
+using VersionR.Areas.Admin.Controllers;
+
+namespace VersionR.Tests.Areas.Admin.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            HomeController controller = new HomeController();
+
+            RedirectToRouteResult result = (RedirectToRouteResult)controller.Index();
+
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("Dashboard", result.RouteValues["controller"]);
+        }
+    }
+}
