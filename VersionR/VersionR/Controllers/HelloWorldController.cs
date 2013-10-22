54c1edf06bcb78da858d38a95e21ae26c07cdb9c
Daniel Clasen <mail@danielclasen.de>
2013-10-22 04:10:08 +0200

    Added email service and email templates for mail senging via smtp mail relay server

--- VersionR/VersionR/Controllers/HelloWorldController.cs
+++ VersionR/VersionR/Controllers/HelloWorldController.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VersionR.Helpers;
+using VersionR.Services;
 
 namespace VersionR.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/ 
+        private readonly VersionR.Models.VersionR _db = new VersionR.Models.VersionR();
+        private readonly DAL.Repositories _repos = new DAL.Repositories();
+
+        //
+        // GET: /HelloWorld/
         public ActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /Welcome/ 
+        //
+        // GET: /Welcome/
         public ActionResult Welcome(string name, int num = 1)
         {
             ViewData["Message"] = "Hello " + name;
@@ -24,8 +29,8 @@ namespace VersionR.Controllers
             return View();
         }
 
-        // 
-        // GET: /Welcome/ 
+        //
+        // GET: /Welcome/
         public ActionResult List(string name, int num = 1)
         {
             ViewData["Message"] = "Hello " + name;
@@ -33,5 +38,36 @@ namespace VersionR.Controllers
             return View();
         }
 
+        public ActionResult Email()
+        {
+            var testUser = _repos.UserRepository.GetByID(1);
+            var testModule = testUser.Customer_Modules.First().Module;
+            var testVersion = testModule.Versions.First();
+            var em = new EmailMessage
+                         {
+                             Subject = "test message",
+                             Template = new NewVersionEmailTemplate(testUser, testModule, testVersion),
+                             From = "noreply@versionr.com",
+                             To = "mail@danielclasen.de"
+                         };
+            if (new EmailService().SendMessage(em,
+                                               "ia211-relay@danielclasen.de",
+                                               "Rc[ln.Pj!sC3bpGT",
+                                               "smtp.1und1.de",
+                                               25,
+                                               false))
+            {
+                TempData["uihint"] = new UiHint("Erfolg!",
+                                                "Die E-Mail wurde erfolgreich versendet!",
+                                                new {@class = "alert alert-success"});
+            }
+            else
+            {
+                TempData["uihint"] = new UiHint("Da ist etwas schief gelaufen!",
+                                                "Die E-Mail konnte nicht gesendet werden!",
+                                                new {@class = "alert alert-error"});
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
---
+++ VersionR/VersionR/Services/EmailService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Hosting;
+using VersionR.Models;
+using Version = VersionR.Models.Version;
+
+namespace VersionR.Services
+{
+    public interface IEmailTemplate
+    {
+        string TemplatePath { get; set; }
+
+        string Body { get; set; }
+
+        bool IsHtml { get; set; }
+
+        Regex Re { get; set; }
+
+        Dictionary<string, string> Fields { get; set; }
+    }
+
+    public class DefaultEmailTemplate : IEmailTemplate
+    {
+        public string TemplatePath { get; set; }
+
+        public string Body { get; set; }
+
+        public bool IsHtml { get; set; }
+
+        public Regex Re { get; set; }
+
+        public Dictionary<string, string> Fields { get; set; }
+
+        private string FileContents { get; set; }
+
+        protected DefaultEmailTemplate(string path = "")
+        {
+            TemplatePath = Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, path);
+            IsHtml = true;
+            Re = new Regex(@"\$(\w+)\$", RegexOptions.Compiled);
+        }
+
+        protected void ReadTemplate()
+        {
+            FileContents = File.ReadAllText(TemplatePath);
+            Body = Re.Replace(FileContents, match => Fields[match.Groups[1].Value]);
+        }
+    }
+
+    public class NewVersionEmailTemplate : DefaultEmailTemplate
+    {
+        public NewVersionEmailTemplate(User user, Module module, Version version)
+            : base("Services/EmailTemplates/NewVersionEmailTemplate.html")
+        {
+            Fields = new Dictionary<string, string>(
+                StringComparer.OrdinalIgnoreCase)
+                         {
+                             {"username", user.NickName},
+                             {"date", DateTime.Now.ToString("d", CultureInfo.CreateSpecificCulture("de-DE"))},
+                             {"modulename", module.Name},
+                             {"newversion", version.Release.ToString()}
+                         };
+
+            ReadTemplate();
+        }
+    }
+
+    public class EmailMessage
+    {
+        public string To { get; set; }
+
+        public string From { get; set; }
+
+        public string Subject { get; set; }
+
+        public IEmailTemplate Template { get; set; }
+    }
+
+    public class EmailService
+    {
+        public bool SendMessage(EmailMessage message, string username, string password, string host, int port,
+                                bool enableSsl)
+        {
+            MailMessage mm = new MailMessage(message.From, message.To, message.Subject, message.Template.Body);
+            if (message.Template.IsHtml)
+                mm.IsBodyHtml = true;
+            NetworkCredential credentials = new NetworkCredential(username, password);
+            SmtpClient sc = new SmtpClient(host, port);
+            sc.EnableSsl = enableSsl;
+            sc.Credentials = credentials;
+
+            try
+            {
+                sc.SendAsync(mm, message.To);
+                return true;
+            }
+            catch (Exception)
+            {
+                //add to logging db
+                return false;
+            }
+        }
+    }
+}
