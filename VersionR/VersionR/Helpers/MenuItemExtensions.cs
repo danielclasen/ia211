05d858983304fb7e0f4c39620f183069bbbb3ab2
Marios Christodoulou <hansnans@gmail.com>
2013-10-31 15:56:04 +0100

    ein bissl Design farbe und Icons eingebracht.

--- VersionR/VersionR/Helpers/MenuItemExtensions.cs
+++ VersionR/VersionR/Helpers/MenuItemExtensions.cs
@@ -21,19 +21,39 @@ namespace VersionR.Helpers
         {
             var routeValues = new { area = areaName };
             var li = GenerateLi(htmlHelper, text, action, controller, areaName);
-            li.InnerHtml = htmlHelper.ActionLink(text, action, controller, routeValues, new { }).ToHtmlString();
+            string id = text.Replace(" ", "").ToLower() + "MenuItem";
+            li.InnerHtml = htmlHelper.ActionLink(text, action, controller, routeValues, new { id=id}).ToHtmlString();
             return MvcHtmlString.Create(li.ToString());
         }
 
+        public static MvcHtmlString MenuItem(this HtmlHelper htmlHelper, string text, string icon, int iconSize, string action, string controller,
+                                             string areaName)
+        {
+            string ret = MenuItem(htmlHelper, text, action, controller, areaName).ToString();
+
+            string[] parts = System.Text.RegularExpressions.Regex.Split(ret, text);
+
+            if (parts != null && parts.Length > 1)
+            {
+                ret = parts[0];
+                ret += string.Format("<i class='icon icon-{0} icon-{1}x'></i> ", icon, iconSize.ToString());
+                ret += text;
+                ret += parts[1];
+            }
+            return MvcHtmlString.Create(ret);
+        }
         private static TagBuilder GenerateLi(this HtmlHelper htmlHelper, string text, string action, string controller,
                                              string areaName = "")
         {
             var li = new TagBuilder("li");
             var routeData = htmlHelper.ViewContext.RouteData;
+
             var currentAction = routeData.GetRequiredString("action");
             var currentController = routeData.GetRequiredString("controller");
             var currentArea = routeData.DataTokens["area"] != null ? routeData.DataTokens["area"].ToString() : "";
-            if ((string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) &&
+
+            if (
+                (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) &&
                  string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase) &&
                  string.Equals(currentArea, areaName, StringComparison.OrdinalIgnoreCase)) ||
                 (string.Equals(action, "Index", StringComparison.OrdinalIgnoreCase) &&
